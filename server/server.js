4f4dc23779405e5cae0c730cf697a4adddbdd9f9
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-12-18 21:24:05 +0700

    feat: add GET method

--- server/server.js
+++ server/server.js
@@ -5,18 +5,33 @@ const mysql = require("mysql2");
 
 app.use(cors());
 
-app.get("/users", function (req, res, next) {
-  connection.query("SELECT * FROM `users`", function (err, results, fields) {
-    res.json(results);
-  });
-});
-
 // create the connection to database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "employee_list",
 });
+
+//read all users information
+app.get("/users", function (req, res, next) {
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    res.json(results);
+  });
+});
+
+//read user information with id
+app.get("/users/:id", function (req, res) {
+  const id = req.params.id;
+  connection.query(
+    "SELECT * FROM `users` WHERE `id` = ?",
+    [id],
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+
 
 app.listen(5000, function () {
   console.log("Web server listening on port 5000");
